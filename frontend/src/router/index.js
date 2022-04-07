c5c3f5f934aa9cdbe4a584a6a2bec1767941f8e3
sonia2605 <sonia.livet@orange.fr>
2022-04-07 12:47:55 +0200

    user

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,33 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import auth from '../middleware/auth.js'
-import axios from 'axios'
-//import VueRouteMiddleware from 'vue-route-middleware'
-
+//import axios from 'axios'
+import VueRouteMiddleware from 'vue-route-middleware'
 
 const routes = [
   {
     path: '/',
-    name: 'HomeVue',
+    name: 'Home',
     component: Home
   },
   {
     path: '/signup',
-    name: 'SignupVue',
-    component: () => import('../views/Signup.vue')
+    name: 'Signup',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
   },
   {
     path: '/post',
-    name: 'PostVue',
-    component: () => import('../views/Post.vue'),
+    name: 'Post',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Post.vue'),
     meta: {
       middleware: auth
     }
   },
   {
     path: '/profile',
-    name: 'ProfileVue',
-    component: () => import('../views/Profile.vue'),
+    name: 'Profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue'),
     meta: {
       middleware: auth
     }
@@ -37,17 +36,19 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-});
+})
 
-router.beforeEach((to, from, next) => { 
-  console.log(to);
-  //if (from.path ('/')){
-  //next();
-  //return;
- // }
+router.beforeEach(VueRouteMiddleware())
+
+export default router
+
+/*=> { 
+  if (to.path == ('')){
+  next("/*")}
+
 if(localStorage.getItem('token')) {
 axios
-  .post("http://localhost:3000/api/user/me",  {},{
+.post("http://localhost:3000/api/user/me",  {},{
   headers: {
     "Content-Type": "application/json",
     Authorization: "Bearer " + localStorage.getItem("token"),
@@ -61,10 +62,9 @@ axios
   })
 } else{
   next({ name: "HomeVue"});
-}
-});
+}})
 
-export default router;
+*/
 
 
 
